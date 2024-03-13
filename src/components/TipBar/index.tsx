25a4fec66edeba02a6428a8b0984b6807f01c2fd
Andre buzelli <buzelli4@gmail.com>
2024-03-13 19:59:44 -0300

    ajuste TipBar

--- src/components/TipBar/index.tsx
+++ src/components/TipBar/index.tsx
@@ -7,37 +7,37 @@ const TipBar = () => {
       <h3 className="w-full text-center mb-[21px] font-[700] text-[16px] text-[#353535]">
         Por que comprar na Maeztra?
       </h3>
-      <div>
-        <div className="px-[16px] flex flex-row flex-nowrap gap-[14px] lg:gap-[16px] overflow-x-scroll lg:justify-center scrollbar-hide">
+      <div className="lg:px-[161px] pl-[31px]">
+        <div className=" max-w-[1598px] flex flex-row flex-nowrap gap-[14px] lg:gap-[15px] overflow-x-scroll scrollbar-hide">
           <TipBarItem
             icon={Icons.earth}
             title="Produtos importados"
             description="Produtos de Alta Qualidade"
-            classNames="w-[298px]"
+            classNames="w-[308px]"
           />
           <TipBarItem
             icon={Icons.house}
             title="Estoque no Brazil"
             description="Produtos mais perto de você!"
-            classNames="w-[298px]"
+            classNames="w-[308px]"
           />
           <TipBarItem
             icon={Icons.devolution}
             title="Trocas Garantidas"
             description="Trocas em até 48 horas, vejas as regras"
-            classNames="w-[298px]"
+            classNames="w-[308px]"
           />
           <TipBarItem
             icon={Icons.discount}
             title="Ganhe 5% off"
             description="Pagando à vista no Cartão"
-            classNames="w-[298px]"
+            classNames="w-[308px]"
           />
           <TipBarItem
             icon={Icons.shipping}
             title="Frete Grátis"
             description="Em compras acima de R$ 499,00"
-            classNames="w-[298px]"
+            classNames="w-[308px]"
           />
         </div>
       </div>
