9c0425c8dc9dd8cc2b7978fc84eaa24ab74a73c8
Andre buzelli <buzelli4@gmail.com>
2024-03-14 11:33:52 -0300

    Add TexImageSection

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import BannerPrincipal from "./components/BannerPrincipal"
 import TipBar from './components/TipBar';
 import Marcas from './components/Marcas';
 import Shelf from './components/Shelf';
+import TextImageSection from './components/TextImageSection';
+import Icons from './components/Icons/icons';
 
 
 function App() {
@@ -11,11 +13,18 @@ function App() {
   return (
     <>
       <div className="font-titillium">
-        <Header/>
-        <BannerPrincipal/>
-        <TipBar/>
-        <Marcas/>
-        <Shelf/>
+        <Header />
+        <BannerPrincipal />
+        <TipBar />
+        <Marcas />
+        <Shelf />
+        <TextImageSection
+          title="Lorem ipsum"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum mauris posuere fusce proin mattis. Hendrerit cras ut nunc enim dictum. Mattis proin ut quis donec sed eget nulla. Vel mi ut mauris integer. Nibh sagittis in lobortis sed cursus condimentum velit pharetra. Amet luctus ut vulputate scelerisque"
+          imageUrl={Icons.bannerNewCollection}
+          imageUrlMob={Icons.bannerNewCollectionMob}
+          alt="banner new collection"
+        />
       </div>
     </>
   )
